5fa234887be0e71d7039bcbcfe8b0e6bc64df74e
Wsmallnews <Wsmallnews@users.noreply.github.com>
2024-12-22 15:28:11 +0000

    Fix styling

--- src/Concerns/RepeaterTree.php
+++ src/Concerns/RepeaterTree.php
@@ -6,7 +6,6 @@ use Filament\Forms\Components;
 
 trait RepeaterTree
 {
-
     public function repeaterField($fields = [], $relation_name = 'children', $name = 'children'): Components\Repeater
     {
         return Components\Repeater::make($name ?: $relation_name)
@@ -16,13 +15,10 @@ trait RepeaterTree
             ->schema($fields);
     }
 
-
     public function fields(): array
     {
         return [];
     }
-
-
 
     public function getFieldsTree($level = 1, $relation_name = 'children', $name = 'children')
     {
@@ -31,8 +27,8 @@ trait RepeaterTree
         if ($level > 1) {
             $fields = array_merge($fields, [
                 Components\Group::make([
-                    $this->repeaterField($this->getFieldsTree($level - 1, $relation_name, $name), $relation_name, $name)
-                ])
+                    $this->repeaterField($this->getFieldsTree($level - 1, $relation_name, $name), $relation_name, $name),
+                ]),
             ]);
         }
 
