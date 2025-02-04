c0e250f8798d93a95725c17b9621aec90a3ea439
Wsmallnews <16216215+Wsmallnews@users.noreply.github.com>
2025-02-04 05:46:30 +0000

    Fix styling

--- src/Traits/Components/CanScopeable.php
+++ src/Traits/Components/CanScopeable.php
@@ -2,17 +2,13 @@
 
 namespace Wsmallnews\Support\Traits\Components;
 
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Livewire\Attributes\Locked;
 
 trait CanScopeable
 {
-
     #[Locked]
     public string $scope_type = 'default';
 
     #[Locked]
     public int $scope_id = 0;
-
 }
