17b6186808fe0287fa9e12d29e04a554bef7454b
smallnews <1371606921@qq.com>
2025-11-09 21:31:03 +0800

    snScope 查询

--- src/Models/SupportModel.php
+++ src/Models/SupportModel.php
@@ -2,10 +2,30 @@
 
 namespace Wsmallnews\Support\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Wsmallnews\Support\Models\Traits\Scopeable;
 
 class SupportModel extends Model
 {
     use Scopeable;
+
+
+    public function scopeScopeTenant(Builder $query): Builder
+    {
+        if (has_tenancy()) {
+            return $query->where('team_id', current_tenant()->id);
+        } else {
+            return $query->whereNull('team_id');
+        }
+    }
+
+
+    /**
+     * 范围查询 和 租户查询合并
+     */
+    public function scopeSnScope(Builder $query, string $scope_type, int | array $scope_id = 0): Builder
+    {
+        return $query->scopeable($scope_type, $scope_id)->scopeTenant();
+    }
 }
