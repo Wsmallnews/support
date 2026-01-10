fa8294ce23b055d4467adcef5d5833454f596fb5
smallnews <1371606921@qq.com>
2026-01-11 00:05:49 +0800

    alert blade

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,36 +1,46 @@
 @props([
-    'title',
-    'description',
-    'color' => 'primary',
+    'title' => null,
+    'description' => null,
+    'color' => 'success',
     'border' => true,
-    'icon',
+    'icon' => null,
+    'iconVerticalAlignment' => 'center',
 ])
 
 @php
-    dd($attributes);
-
     use function Filament\Support\get_color_css_variables;
     $colors = \Illuminate\Support\Arr::toCssStyles([
         get_color_css_variables($color, shades: [50, 100, 400, 500, 700, 800]),
     ]);
+
+    if (!$icon) {
+        match ($color) {
+            'primary' => $icon = 'heroicon-o-check-circle',
+            'danger' => $icon = 'heroicon-s-x-circle',
+            'info' => $icon = 'heroicon-s-information-circle',
+            'success' => $icon = 'heroicon-s-check-circle',
+            'warning' => $icon = 'heroicon-s-exclamation-triangle',
+            default => $icon = 'heroicon-o-check-circle',
+        };
+    }
 @endphp
 
 <div x-data="{}"
     {{ 
         $attributes->class([
-            'sn-support-session-flash rounded-md bg-custom-50 p-4 dark:bg-custom-400/10',
+            'sn-support-alert rounded-md bg-custom-50 p-4 dark:bg-custom-400/10',
             'ring-1 ring-custom-100 dark:ring-custom-500/70' => $border,
-        ]) 
+        ])
     }}
     style="{{ $colors }}">
     <div class="flex gap-3">
         @if($icon)
             <div @class([
                 'flex-shrink-0',
-                $getIconVerticalAlignment() === 'start' ? 'self-start' : 'self-center',
+                $iconVerticalAlignment === 'start' ? 'self-start' : 'self-center',
             ])>
                 <x-filament::icon
-                    :icon="$icon()"
+                    :icon="$icon"
                     class="h-5 w-5 text-custom-400"
                 />
             </div>
