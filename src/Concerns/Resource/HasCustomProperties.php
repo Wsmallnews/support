c227cdc80bfeae342a9d0a4453d8648b9efad557
smallnews@qq.com <1371606921@qq.com>
2026-01-30 18:51:14 +0800

    支持自定义 resource form

--- src/Concerns/Resource/HasCustomProperties.php
+++ src/Concerns/Resource/HasCustomProperties.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace Wsmallnews\Support\Concerns\Resource;
 
 use BezhanSalleh\PluginEssentials\Concerns\Resource\DelegatesToPlugin;
+use Closure;
+use Filament\Schemas\Schema;
+use Filament\Tables\Table;
 
 trait HasCustomProperties
 {
@@ -24,5 +27,71 @@ trait HasCustomProperties
     public static function getCustomProperty(string $key): mixed
     {
         return static::getCustomProperties()[$key] ?? null;
+    }
+
+    /**
+     * 快捷获取自定义的 table
+     *
+     * @param Table $table
+     * @return Table|null
+     */
+    public static function getCustomTable(Table $table): ?Table
+    {
+        return self::getCustomProperty('table') instanceof Closure ?
+            self::getCustomProperty('table')($table, self::class)
+            : null;
+    }
+
+    /**
+     * 快捷获取自定义的 form schema
+     *
+     * @param Schema $schema
+     * @return Schema|null
+     */
+    public static function getCustomForm(Schema $schema): ?Schema
+    {
+        return self::getCustomProperty('form') instanceof Closure ?
+            self::getCustomProperty('form')($schema, self::class)
+            : null;
+    }
+
+    /**
+     * 快捷获取自定义的 form array 数组 (比如：filament-nestedset 插件)
+     *
+     * @param Array $arguments
+     * @return Array|null
+     */
+    public static function getCustomFormArray(Array $arguments): ?Array
+    {
+        return self::getCustomProperty('form_array') instanceof Closure ?
+            self::getCustomProperty('form_array')($arguments, self::class)
+            : null;
+    }
+
+
+    /**
+     * 快捷获取自定义的 infolist schema
+     *
+     * @param Schema $schema
+     * @return Schema|null
+     */
+    public static function getCustomInfolist(Schema $schema): ?Schema
+    {
+        return self::getCustomProperty('infolist') instanceof Closure ?
+            self::getCustomProperty('infolist')($schema, self::class)
+            : null;
+    }
+
+    /**
+     * 快捷获取自定义的 infolist array 数组 (比如：filament-nestedset 插件)
+     *
+     * @param Array $arguments
+     * @return Array|null
+     */
+    public static function getCustomInfolistArray(): ?array
+    {
+        return self::getCustomProperty('infolist_array') instanceof Closure ?
+            self::getCustomProperty('infolist_array')(self::class)
+            : null;
     }
 }
