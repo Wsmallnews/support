0cac0b09f110570ea2a3a7e3a7be3d2689a2a301
smallnews <1371606921@qq.com>
2026-05-03 22:41:17 +0800

    支持 markdown , 新增 可折叠内容 blade 模板，新增 markdown 样式，新增 可折叠内容 样式

---
+++ resources/views/components/collapse-content.blade.php
@@ -0,0 +1,56 @@
+@props([
+    'content' => null,                    // 内容
+    'contentType' => 'markdown',          // 内容类型，  markdown or richtext
+    'maxHeight' => 350                    // 默认内容高度
+])
+
+<div
+    class="sn-collapse-container"
+    x-data="{
+        collapsed: false,
+        needCollapse: false,
+        init() {
+            $nextTick(() => {
+                const el = this.$refs.content;
+                this.needCollapse = el.scrollHeight > {{ $maxHeight }};
+                this.collapsed = this.needCollapse;
+            });
+        },
+        toggle() {
+            this.collapsed = !this.collapsed;
+        }
+    }"
+>
+    <div
+        x-ref="container"
+        x-cloak
+        class="sn-collapse-content"
+        :class="collapsed ? 'sn-collapsed' : 'sn-expanded'"
+        :style="collapsed ? 'max-height: {{ $maxHeight }}px' : 'max-height: none'"
+    >
+        <div x-ref="content">
+            @if ($contentType == 'markdown')
+                <x-markdown class="sn-markdown-content">
+                    {!! $content !!}
+                </x-markdown>
+            @else
+                {!! $content !!}
+            @endif
+        </div>
+        <div
+            class="sn-collapse-fade"
+            x-show="collapsed"
+            x-transition.opacity.duration.100ms
+        ></div>
+    </div>
+    <div
+        x-show="needCollapse"
+        x-cloak
+        class="sn-collapse-toggle"
+        @click="toggle()"
+    >
+        <span x-text="collapsed ? '展开全部' : '收起'"></span>
+        <x-filament::icon x-show="collapsed" icon="heroicon-m-chevron-down" class="size-4 font-semibold" />
+        <x-filament::icon x-show="!collapsed" icon="heroicon-m-chevron-up" class="size-4 font-semibold" />
+    </div>
+</div>
--- src/Livewire/Concerns/HasEditorType.php
+++ src/Livewire/Concerns/HasEditorType.php
@@ -9,4 +9,13 @@ trait HasEditorType
 
     public EditorType $editorType = EditorType::Textarea;
 
+
+    /**
+     * 是否是 格式化编辑器
+     */
+    public function isFormattedEditor(): bool
+    {
+        return in_array($this->editorType, [EditorType::RichEditor, EditorType::MarkdownEditor]);
+    }
+
 }
