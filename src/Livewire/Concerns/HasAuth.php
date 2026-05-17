99a81f7de2bce84167733ed262dee741f065b651
smallnews <1371606921@qq.com>
2026-05-17 18:09:29 +0800

    更新 HasAuth 可以手动设置认证用户

--- src/Livewire/Concerns/HasAuth.php
+++ src/Livewire/Concerns/HasAuth.php
@@ -2,6 +2,7 @@
 
 namespace Wsmallnews\Support\Livewire\Concerns;
 
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
 
 trait HasAuth
@@ -9,13 +10,29 @@ trait HasAuth
     /**
      * 当前认证用户
      */
-    public ?Model $user = null;         // 当前认证用户
+    public Model | Authenticatable | null $user = null;         // 当前认证用户
 
-    public function getAuthUser(): ?Model
+    /**
+     * 设置当前认证用户
+     */
+    public function authUser(Model | Authenticatable | null $user): static
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * 获取当前认证用户
+     */
+    public function getAuthUser(): Model | Authenticatable | null
     {
         return $this->user;
     }
 
+    /**
+     * 是否存在认证用户
+     */
     public function hasAuthUser(): bool
     {
         return $this->user !== null;
