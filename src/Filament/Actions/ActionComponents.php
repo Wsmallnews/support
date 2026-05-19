e1bd427f33ef0de95fef2542344755d19e215541
Wsmallnews <16216215+Wsmallnews@users.noreply.github.com>
2026-05-19 10:41:29 +0000

    Fix styling

--- src/Filament/Actions/ActionComponents.php
+++ src/Filament/Actions/ActionComponents.php
@@ -11,7 +11,7 @@ class ActionComponents
     {
         return Action::make($name)
             ->label(__('filament-actions::delete.single.label'))
-            ->modalHeading(fn(): string => __('filament-actions::delete.single.modal.heading', ['label' => $label]))
+            ->modalHeading(fn (): string => __('filament-actions::delete.single.modal.heading', ['label' => $label]))
             ->modalSubmitActionLabel(__('filament-actions::delete.single.modal.actions.delete.label'))
             ->successNotificationTitle(__('filament-actions::delete.single.notifications.deleted.title'))
             ->failureNotificationTitle(__('sn-support::support.action.delete_failed'))
@@ -23,12 +23,11 @@ class ActionComponents
             ->keyBindings(['mod+d']);
     }
 
-
     public static function editAction(string $name = 'edit', ?string $label = null): Action
     {
         return Action::make($name)
             ->label(__('filament-actions::edit.single.label'))
-            ->modalHeading(fn(): string => __('filament-actions::edit.single.modal.heading', ['label' => $label]))
+            ->modalHeading(fn (): string => __('filament-actions::edit.single.modal.heading', ['label' => $label]))
             ->modalSubmitActionLabel(__('filament-actions::edit.single.modal.actions.save.label'))
             ->successNotificationTitle(__('filament-actions::edit.single.notifications.saved.title'))
             ->defaultColor('primary')
